e7a333d2c444fd0bd2d7cbdadb0c6b2199d73c84
vivi-shiwei <2546710985@qq.com>
2020-07-21 11:56:28 +0800

    7-21

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // stories: ['../stories/**/*.[tj]s'],
-  stories: ['../stories/**/footer.[tj]s', '../stories/**/hero.[tj]s', '../stories/**/header.[tj]s'],
+  stories: ['../stories/**/*.[tj]s'],
+  // stories: ['../stories/**/footer.[tj]s', '../stories/**/hero.[tj]s', '../stories/**/header.[tj]s'],
   addons: ['@storybook/addon-knobs/register']
 };
--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 這是一個引用了chakra-ui框架的一個npm
 
-# 观看网址
+# 观看效果网址
 https://viviui.vercel.app/?path=/story/footer--footer
+
+# Header使用
