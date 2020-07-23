d8d284b910ec15c747e74254bf457923547cfd6b
vivi-shiwei <2546710985@qq.com>
2020-07-23 10:37:00 +0800

    横向修改完毕

--- src/components/cardDeck/index.js
+++ src/components/cardDeck/index.js
@@ -5,9 +5,10 @@ import Title from './title'
 const CardDeck = ({ title, topLeft, cardButtom, chidren, ...props }) => {
   return (
     <Box maxW='960px' mx='auto' p='20px' {...props}>
+      {/* <Title title={title} /> */}
       <Box d='flex' justifyContent='center'>
         {topLeft}
-        {!!title && (<Title title={title} background='red' />)}
+        {!!title && (<Title title={title} />)}
       </Box>
       <Box>
         {cardButtom}
--- src/components/cardDeck/title.js
+++ src/components/cardDeck/title.js
@@ -1,28 +1,22 @@
 import React from 'react'
-import { Box, PseudoBox, Divider, Text, Heading } from '@chakra-ui/core'
+import { Box, PseudoBox, Divider, Text, Heading, Flex } from '@chakra-ui/core'
 
 const Title = ({ title, chidren, ...props }) => {
   return (
     <>
-      <Box width='full' textAlign='center' {...props}>
-        <fieldset border='1px solid black' borderButton='none' borderLeft='none' borderRight='none' position='relative'>
-          <legend> {title}</legend>
-          {/* <Box position='absolute' top='-3px' left='0px'>左边</Box>
-          <Box position='absolute' top='-3px' right='0px'>右边</Box> */}
-        </fieldset>
+      <Box position='relative' width='full' {...props}>
+        <Flex justify='center' align='center'>
+          <Box borderBottom='1px solid #aaa6a6' w='100%' position='absolute' zIndex='0' />
+          <Heading fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }} position='relative' textAlign='center' bg='white' color='black' lineHeight='40px' p='0 5px'>
+            {title}
+          </Heading>
+        </Flex>
       </Box>
-      {/* <Box display='flex' alignItems='center' justifyContent='center' width='full'>
-        <Text textAlign='left' display='inline-block' width='45%' borderTop='1px solid #ccc' />
-        <Text verticalAlign='middle' width='10%' fontSize='1.3em' color='black' fontSize='30px' fontWeight='bold' textAlign='center'>班级</Text>
-        <Text textAlign='right' display='inline-block' width='45%' borderTop='1px solid #ccc' />
-      </Box> */}
-
       {/* <Box width='100%' position='relative' textAlign='center'>
         <Divider borderColor='#b7b9bb' position='absolute' top='12px' left='0' width={{ base: '25%', md: '40%' }} />
         <Text fontSize={{ base: '24px', sm: '24px', md: '24px' }} lineHeight='40px' fontWeight='bold' ml={{ base: '-30px', sm: '-30px', md: '0' }}> {title}</Text>
         <Divider borderColor='#b7b9bb' position='absolute' top='12px' right='0' width={{ base: '35%', sm: '35%', md: '40% ' }} />
       </Box> */}
-
     </>
   )
 }
