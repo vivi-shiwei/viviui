bfcc810f78be35a2a3fe92a755cde3145f235819
vivi-shiwei <2546710985@qq.com>
2020-07-24 12:09:25 +0800

    卡片

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box } from '@chakra-ui/core'
 import Fieldset from './fieldset'
 import { Container } from '../containerPage'
-import GroupCards from './groupCards'
+import GroupCards from './groupCard'
 
 const CardDeck = ({ title, topLeft, groupCard, children, ...props }) => {
   return (
