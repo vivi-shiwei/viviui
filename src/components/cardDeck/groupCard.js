98c0112c1ae33288e591be80ba8694a40bc61f72
vivi-shiwei <2546710985@qq.com>
2020-07-24 11:15:49 +0800

    卡片组

--- src/components/cardDeck/groupCard.js
+++ src/components/cardDeck/groupCard.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Grid } from '@chakra-ui/core'
-import Cards from './card'
+import { Grid, Box } from '@chakra-ui/core'
 
-const GroupCard = ({ data, children, ...props }) => {
+const GroupCard = ({ children, ...props }) => {
   return (
     <>
-      <Grid templateColumns={{ base: 'repeat(2, 1fr)', sm: 'repeat(2, 1fr)', md: 'repeat(4, 1fr)' }} gap={6} m='4rem 10px' {...props}>
-        {!!data && data.map((item, index) => {
-          return <><Cards title={item} /></>
-        }
-        )}
+      {/* <Grid templateColumns={{ base: 'repeat(2, 1fr)', sm: 'repeat(2, 1fr)', md: 'repeat(4, 1fr)' }} gap={6} m='4rem 10px' justifyContent='center' {...props}>
         {children}
-      </Grid>
+      </Grid> */}
+      <Box as='section' textAlign='center' mt={3}>
+        {children}
+      </Box>
     </>
 
   )
