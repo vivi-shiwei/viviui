fb74e075b68639f74631caa94528139f6ae827f2
vivi-shiwei <2546710985@qq.com>
2020-07-27 14:23:29 +0800

    7-27

--- src/components/newModal/modalContent.js
+++ src/components/newModal/modalContent.js
@@ -6,13 +6,13 @@ import {
   ModalFooter
 } from '@chakra-ui/core'
 
-const ModalWithContent = ({ mdalHeader, closeButton, modalWithBody, isOpen, onClose, modalWithFooter, children, ...rest }) => {
+const ModalWithContent = ({ mdalHeader, closeButton, modalWithBody, isOpen, onClose, modalWithFooter, children, modalbodyProps, ...rest }) => {
   return (
     <>
       <ModalContent h={{ base: '100%', sm: 'auto' }} maxH='100%' my={{ base: 0, sm: '3.75rem' }} {...rest}>
         {mdalHeader}
         {!!closeButton && <ModalCloseButton />}
-        <ModalBody pb={6}>
+        <ModalBody pb={6} {...modalbodyProps}>
           {modalWithBody}
           {children}
         </ModalBody>
