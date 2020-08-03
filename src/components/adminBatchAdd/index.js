f025b2cfcfa7c526481cabbc2043b003a2c7b8f7
pengchihui1 <pengchihui1@gmail.com>
2020-08-03 18:49:16 +0800

    as

--- src/components/adminBatchAdd/index.js
+++ src/components/adminBatchAdd/index.js
@@ -5,7 +5,15 @@ import { Heading, Box } from '@chakra-ui/core'
 const AddminBatchAdd = ({ title, body, children, ...props }) => {
   return (
     <HeaderContainer pb={4} px={0} maxW='960px' {...props}>
-
+      <Heading
+        as='h2'
+        textAlign='center'
+        size='lg'
+        overflow='hidden'
+        textOverflow='ellipsis'
+        whiteSpace='nowrap'
+      > {title}
+      </Heading>
       <Box d='flex' textAlign='center' flexDirection='column' mt={5}>
         {body}
       </Box>
