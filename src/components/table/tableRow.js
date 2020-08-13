16552209d75f64e5576116351b373c45631a3f47
vivi-shiwei <2546710985@qq.com>
2020-08-13 19:27:32 +0800

    修改数据

--- src/components/table/tableRow.js
+++ src/components/table/tableRow.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box } from '@chakra-ui/core'
 
-// 包装th或者td，让他们不会换行的tr标签
+// 包装th或者td，让他们不会换行的 tr 标签
 const TableRow = ({ children, ...props }) => {
   return (
     <Box
