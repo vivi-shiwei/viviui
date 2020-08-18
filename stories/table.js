3f2415c0cc6f98041068ea0024c6123107d8d493
pengchihui1 <pengchihui1@gmail.com>
2020-08-18 10:45:05 +0800

    test

--- stories/table.js
+++ stories/table.js
@@ -134,7 +134,7 @@ export const table = () => {
           <TableRow>
             <TableCell w='50px'>序號</TableCell>
             <TableCell>名稱</TableCell>
-            <TableCell>值</TableCell>
+            <TableCell>值gffgjh</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
