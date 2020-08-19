97caf61175b70a7a4522945bc85c3305157b34cf
pengchihui1 <pengchihui1@gmail.com>
2020-08-19 18:31:15 +0800

    部分修改

--- src/components/postGroup/index.js
+++ src/components/postGroup/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {
   Box
-
 } from '@chakra-ui/core'
 
 const PostCard = ({ topLeft, topRight, postTalent, postTags, postTitle, postContent, photosGrid, children, ...props }) => {
