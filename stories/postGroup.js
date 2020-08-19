ebe74a440366c6cbadffddf3582a4c30459e7992
pengchihui1 <pengchihui1@gmail.com>
2020-08-19 19:04:41 +0800

    待完善每个compunent传参 及贴文用来展示的图布局

--- stories/postGroup.js
+++ stories/postGroup.js
@@ -11,7 +11,7 @@ import PostTalent from '../src/components/postGroup/postTalent'
 import PostTags from '../src/components/postGroup/postTags'
 import PostTitle from '../src/components/postGroup/postTitle'
 import PostContent from '../src/components/postGroup/postContent'
-// import PhotosGrid from '../src/components/postGroup/photosGrid'
+import PhotosGrid from '../src/components/postGroup/photosGrid'
 
 export default {
   title: 'PostGroup(hui)'
@@ -20,12 +20,12 @@ export default {
 export const postHeader = () => {
   const data = [
     'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597813527234&di=82d265128b9adf3197c66f68a634b16f&imgtype=0&src=http%3A%2F%2Fpic33.photophoto.cn%2F20141030%2F0011024093543268_b.jpg',
-    'https://f10.baidu.com/it/u=1911995130,2805437140&fm=76',
-    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597813527236&di=a5494e7acd2a67875a6a6f40853d87ed&imgtype=0&src=http%3A%2F%2Fn1-q.mafengwo.net%2Fs11%2FM00%2F2F%2F8F%2FwKgBEFq1tuyAWL_MAHFIKtUcCPo65.jpeg',
-    'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1873101598,2655143176&fm=26&gp=0.jpg',
-    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597813641438&di=dba0f784301c7df3daf4f313a8635de8&imgtype=0&src=http%3A%2F%2Fimg3.imgtn.bdimg.com%2Fit%2Fu%3D3014066488%2C1993301398%26fm%3D214%26gp%3D0.jpg',
-    'https://t11.baidu.com/it/u=30595192,1654543529&fm=76',
-    'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2494239978,1960683224&fm=15&gp=0.jpg'
+    'https://f10.baidu.com/it/u=1911995130,2805437140&fm=76'
+    // 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597813527236&di=a5494e7acd2a67875a6a6f40853d87ed&imgtype=0&src=http%3A%2F%2Fn1-q.mafengwo.net%2Fs11%2FM00%2F2F%2F8F%2FwKgBEFq1tuyAWL_MAHFIKtUcCPo65.jpeg',
+    // 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1873101598,2655143176&fm=26&gp=0.jpg',
+    // 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597813641438&di=dba0f784301c7df3daf4f313a8635de8&imgtype=0&src=http%3A%2F%2Fimg3.imgtn.bdimg.com%2Fit%2Fu%3D3014066488%2C1993301398%26fm%3D214%26gp%3D0.jpg',
+    // 'https://t11.baidu.com/it/u=30595192,1654543529&fm=76',
+    // 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2494239978,1960683224&fm=15&gp=0.jpg'
   ]
   return (
     <Box>
@@ -37,7 +37,7 @@ export const postHeader = () => {
         postTags={<PostTags />}
         postTitle={<PostTitle />}
         postContent={<PostContent content='图片标题1 图片标题1 图片标题1' />}
-      // photosGrid={<PhotosGrid />}
+        photosGrid={<PhotosGrid photos={data} />}
       />
     </Box>
   )
