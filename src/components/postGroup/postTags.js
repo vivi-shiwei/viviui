6e020e93b8f6f0734c5d3f8100ab14779195c5cc
pengchihui1 <pengchihui1@gmail.com>
2020-08-20 15:57:56 +0800

    标注可以了但不能点击

--- src/components/postGroup/postTags.js
+++ src/components/postGroup/postTags.js
@@ -5,11 +5,18 @@ import {
 import { FaUserTag } from 'react-icons/fa'
 
 const PostTags = ({ children, ...props }) => {
+  const count = children.length
   return (
     <Box mt={3} d='flex' alignItems='center' flexWrap='wrap' {...props}>
       <Box as={FaUserTag} fontSize='20px' w='20px' mr={2} />
       {children.map((item, index) => {
-        return item
+        const name = item.props.children
+        const name2 = name.concat('、')
+        if (count !== index + 1) {
+          return name2
+        } else {
+          return name
+        }
       })}
     </Box>
   )
