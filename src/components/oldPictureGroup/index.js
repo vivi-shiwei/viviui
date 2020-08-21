a378ca64b29d16b7d7a2aa87b67684dda8be999d
pengchihui1 <pengchihui1@gmail.com>
2020-08-21 09:45:35 +0800

    初步创建oldPictureGroup文件

---
+++ src/components/oldPictureGroup/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  Box
+} from '@chakra-ui/core'
+
+const OldPictureGroup = ({ children, ...props }) => {
+  // const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Box>123</Box>
+    </>
+  )
+}
+
+export default OldPictureGroup
---
+++ stories/oldPictureGroup.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Box } from '@chakra-ui/core'
+import Container from '../src/components/container'
+
+export default { title: 'OldPictureGroup(hui)' }
+
+export const OldPictureGroup = () => {
+  const data = [
+    'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2534295925,1741655192&fm=26&gp=0.jpg',
+    'https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=390752696,937398701&fm=26&gp=0.jpg',
+    'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3679156262,2063123018&fm=26&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1815132930,1669392629&fm=26&gp=0.jpg',
+    'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2416085737,1631868062&fm=26&gp=0.jpg',
+    'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3193828568,4262493738&fm=26&gp=0.jpg',
+    'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1896313333,2910419514&fm=26&gp=0.jpg'
+  ]
+  return (
+    <Container mt='30px'>
+      <Box>
+        456
+      </Box>
+    </Container>
+  )
+}
