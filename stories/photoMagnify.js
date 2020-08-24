c96a5f0b99e682b2dcb7da197632e68f069e70d8
pengchihui1 <pengchihui1@gmail.com>
2020-08-24 23:01:13 +0800

    左右按键慢速点击可行，modal可打开关闭

--- stories/photoMagnify.js
+++ stories/photoMagnify.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Box, useDisclosure, Button } from '@chakra-ui/core'
+import { 
+	Box,
+	useDisclosure,
+	Button ,
+	Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+	Text
+  } from '@chakra-ui/core'
 
 import PhotoMagnify from '../src/components/photoMagnify'
 
 export default { title: 'PhotoMagnify(hui)' }
 
 const darks = [
-  // 'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3988286801,48626279&fm=26&gp=0.jpg',
+  'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3988286801,48626279&fm=26&gp=0.jpg',
   'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1947839005,1811220579&fm=26&gp=0.jpg',
   'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3383916863,1183745965&fm=26&gp=0.jpg',
   'https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1187213634,3203010965&fm=26&gp=0.jpg',
@@ -16,15 +28,13 @@ const darks = [
 ]
 
 export const PhotoMagnifyTest = () => {
-  // const [isOpenPhotoViewModal, onOpenPhotoViewModal] = useState(null)
-  // const onClosePhotoViewModal = () => onOpenPhotoViewModal(null)
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const show = darks.slice(0, 1)
+  const show = darks.slice(0, 2)
   return (
     <>
       <Box>
-        <Button onClick={onOpen}>Open Modal</Button>
-        <PhotoMagnify images={darks} indexI={darks.length} isOpen={onOpen} onClose={onClose} />
+       <Button onClick={onOpen}>Open Modal</Button>
+       <PhotoMagnify images={show} indexI={show.length} isOpen={isOpen} onClose={onClose}/>
       </Box>
     </>
   )
