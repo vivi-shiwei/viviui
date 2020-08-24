2f5d3e0b3b33a8247ccbe89da98dffbadcf8e9ed
pengchihui1 <pengchihui1@gmail.com>
2020-08-24 14:08:49 +0800

    三四五张

--- src/components/postCardTwo/index.js
+++ src/components/postCardTwo/index.js
@@ -34,7 +34,9 @@ const PictureBox = ({ children, ...props }) => {
     <Box {...props}>
       <Box mt='10px' mx={{ base: 'auto', sm: '-13px' }}>
         {children.length < 2 && (
-          a
+          <Box>
+            {a}
+          </Box>
         )}
 
         {children.length === 2 && (
@@ -52,97 +54,28 @@ const PictureBox = ({ children, ...props }) => {
           </Box>
         )}
 
-        {/* {children.length === 1 && (
-          children
+        {children.length === 3 && (
+          <Box d='flex' maxW='100%' maxH='50%'>
+            <Box as='article' width={['50%', '50%', '49.8%', '33.8%']} h='50%' m='0 1px 0 0' border='1px solid #b7b7b7'>
+              <AspectRatioBox h='50%' ratio={1}>
+                {a}
+              </AspectRatioBox>
+            </Box>
+            <Box as='article' w={['50%', '50%', '49.8%', '33.8%']} h='50%' m='0 1px 0 0' border='1px solid #b7b7b7'>
+              <AspectRatioBox h='50%' ratio={1}>
+                {b}
+              </AspectRatioBox>
+            </Box>
+            <Box as='article' w={['50%', '50%', '49.8%', '33.8%']} h='50%' m='0 0 0 1px' border='1px solid #b7b7b7'>
+              <AspectRatioBox h='50%' ratio={1}>
+                {c}
+              </AspectRatioBox>
+            </Box>
+          </Box>
         )}
-        {children.length === 2 && (
-          <Box d='flex' maxW='655px' maxH='655px'>
-            {children.map((c, i) => {
-              return ( */}
-        {/* 两张是长图片时
-                 <>
-                  {i === 0 && ( */}
-        {/* <Box as='article' w={['50%', '50%', '50%', '50%']} m='0 1px 0 0' border='1px solid #b7b7b7'>
-                      {c}
-                    </Box>
-                  )}
-                   {i === 1 && ( */}
-        {/* //     <Box as='article' w={['50%', '50%', '50%', '50%']} m='0 0 0 1px' border='1px solid #b7b7b7'>
-                //       {c}
-                //     </Box>
-                //   )}
-                // </>
-
-                // 两张都是短图片
-                // <>
-                //   {i === 0 && ( */}
-        {/* //     <AspectRatioBox h='50%' ratio={4 / 2} m='0 0 1px 0' border='1px solid #b7b7b7'>
-                //       {c}
-                //     </AspectRatioBox>
-                //   )}
-                //   {i === 1 && ( */}
-        {/* //     <AspectRatioBox h='50%' ratio={4 / 2} m='1px 0 0 0' border='1px solid #b7b7b7'>
-                //       {c}
-                //     </AspectRatioBox>
-                //   )}
-                // </>
-
-                // 一长一短
-        //         <>
-        //           {i === 0 && ( */}
-        {/* //             <Box as='article' w={['50%', '50%', '50%', '50%']} h='50%' m='0 1px 0 0' border='1px solid #b7b7b7'>
-        //               <AspectRatioBox h='50%' ratio={1}>
-        //                 {c}
-        //               </AspectRatioBox>
-        //             </Box>
-        //           )}
-        //           {i === 1 && ( */}
-        {/* //             <Box as='article' w={['50%', '50%', '50%', '50%']} h='50%' m='0 0 0 1px' border='1px solid #b7b7b7'>
-        //               <AspectRatioBox h='50%' ratio={1}>
-        //                 {c}
-        //               </AspectRatioBox>
-        //             </Box>
-        //           )}
-        //         </>
-        //       )
-        //     })}
-        //   </Box> */}
-        {/* // )}
-
-        // {children.length === 3 && ( */}
-        {/* //   <Box d='flex' flexWrap='wrap' maxW='655px' maxH='655px'>
-        //     {children.map((c, i) => { */}
-        {/* //       return (
-        //         <>
-        //           {i === 0 && ( */}
-        {/* //             <Box as='article' width={{ base: '100%', md: '653px' }} h='50%' border='1px solid #b7b7b7'>
-        //               <AspectRatioBox width={{ base: '100%', md: '653px' }} h='50%' ratio={4 / 2}>
-        //                 {c}
-        //               </AspectRatioBox>
-        //             </Box>
-        //           )}
-        //           {i === 1 && ( */}
-        {/* //             <Box as='article' w={['50%', '50%', '49.8%', '49.8%']} h='50%' m='0 1px 0 0' border='1px solid #b7b7b7'>
-        //               <AspectRatioBox h='50%' ratio={1}>
-        //                 {c}
-        //               </AspectRatioBox>
-        //             </Box>
-        //           )}
-        //           {i === 2 && ( */}
-        {/* //             <Box as='article' w={['50%', '50%', '49.8%', '49.8%']} h='50%' m='0 0 0 1px' border='1px solid #b7b7b7'>
-        //               <AspectRatioBox h='50%' ratio={1}>
-        //                 {c}
-        //               </AspectRatioBox>
-        //             </Box>
-        //           )}
-        //         </>
-        //       )
-        //     })}
-        //   </Box> */}
-        {/* // )} */}
 
         {children.length === 4 && (
-          <Box flexWrap='wrap' maxW='655px' maxH='655px'>
+          <Box flexWrap='wrap'>
             <Box d='flex' maxW='100%' maxH='50%'>
               <Box as='article' width={['50%', '50%', '49.8%', '33.8%']} h='50%' m='0 1px 0 0' border='1px solid #b7b7b7'>
                 <AspectRatioBox h='50%' ratio={1}>
@@ -169,6 +102,43 @@ const PictureBox = ({ children, ...props }) => {
             </Box>
           </Box>
         )}
+
+        {children.length === 5 && (
+          <Box flexWrap='wrap'>
+            <Box d='flex' maxW='100%' maxH='50%'>
+              <Box as='article' width={['50%', '50%', '49.8%', '33.8%']} h='50%' m='0 1px 0 0' border='1px solid #b7b7b7'>
+                <AspectRatioBox h='50%' ratio={1}>
+                  {a}
+                </AspectRatioBox>
+              </Box>
+              <Box as='article' w={['50%', '50%', '49.8%', '33.8%']} h='50%' m='0 1px 0 0' border='1px solid #b7b7b7'>
+                <AspectRatioBox h='50%' ratio={1}>
+                  {b}
+                </AspectRatioBox>
+              </Box>
+              <Box as='article' w={['50%', '50%', '49.8%', '33.8%']} h='50%' m='0 0 0 1px' border='1px solid #b7b7b7'>
+                <AspectRatioBox h='50%' ratio={1}>
+                  {c}
+                </AspectRatioBox>
+              </Box>
+            </Box>
+            <Box mt='5px'>
+              <Box d='flex'>
+                <Box as='article' w={['50%', '50%', '50%', '50%']} m='0 0 0 1px'>
+                  <AspectRatioBox ratio={1} m='1px 0 0 0' border='1px solid #b7b7b7'>
+                    {d}
+                  </AspectRatioBox>
+                </Box>
+                <Box as='article' w={['50%', '50%', '50%', '50%']} m='0 0 0 1px'>
+                  <AspectRatioBox ratio={1} m='1px 0 0 0' border='1px solid #b7b7b7'>
+                    {e}
+                  </AspectRatioBox>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        )}
+
       </Box>
     </Box>
   )
--- stories/postCardTwo.js
+++ stories/postCardTwo.js
@@ -4,34 +4,16 @@ import { Box } from '@chakra-ui/core'
 import PostCardTwo from '../src/components/postCardTwo'
 import Pictures from '../src/components/postCardTwo/pictures'
 
-import MyImage from '../static/QQ20200518181405.jpg'
-import MyImage6 from '../static/timg.jpg'
-
 export default { title: 'PostCardTwo(hui)' }
 
 const darks = [
   'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3988286801,48626279&fm=26&gp=0.jpg',
-  'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1947839005,1811220579&fm=26&gp=0.jpg'
-  // 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3383916863,1183745965&fm=26&gp=0.jpg',
-  // 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3383916863,1183745965&fm=26&gp=0.jpg',
-  // 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2407081651,3186896498&fm=26&gp=0.jpg',
+  'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1947839005,1811220579&fm=26&gp=0.jpg',
+  'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3383916863,1183745965&fm=26&gp=0.jpg',
+  'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3383916863,1183745965&fm=26&gp=0.jpg',
+  'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2407081651,3186896498&fm=26&gp=0.jpg'
   // 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=325461855,3402280707&fm=26&gp=0.jpg'
 ]
-
-export const PictureAll = () => {
-  const show = darks
-  return (
-    <>
-      <Box maxW='500px' m='0 auto'>
-        <PostCardTwo>
-          {show.map((item, index) => {
-            return <Pictures key={index} imageURL={item} />
-          })}
-        </PostCardTwo>
-      </Box>
-    </>
-  )
-}
 
 export const PictureOne = () => {
   const show = darks.slice(0, 1)
@@ -50,6 +32,49 @@ export const PictureOne = () => {
 
 export const PictureTwo = () => {
   const show = darks.slice(0, 2)
+  return (
+    <>
+      <Box maxW='500px' m='0 auto'>
+        <PostCardTwo>
+          {show.map((item, index) => {
+            return <Pictures key={index} imageURL={item} />
+          })}
+        </PostCardTwo>
+      </Box>
+    </>
+  )
+}
+
+export const PictureThree = () => {
+  const show = darks.slice(0, 3)
+  return (
+    <>
+      <Box maxW='500px' m='0 auto'>
+        <PostCardTwo>
+          {show.map((item, index) => {
+            return <Pictures key={index} imageURL={item} />
+          })}
+        </PostCardTwo>
+      </Box>
+    </>
+  )
+}
+export const PictureFour = () => {
+  const show = darks.slice(0, 4)
+  return (
+    <>
+      <Box maxW='500px' m='0 auto'>
+        <PostCardTwo>
+          {show.map((item, index) => {
+            return <Pictures key={index} imageURL={item} />
+          })}
+        </PostCardTwo>
+      </Box>
+    </>
+  )
+}
+export const PictureFive = () => {
+  const show = darks.slice(0, 5)
   return (
     <>
       <Box maxW='500px' m='0 auto'>
