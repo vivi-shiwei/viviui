8b0b7762f10c01b588b3bab32eeee191c9677868
pengchihui1 <pengchihui1@gmail.com>
2020-08-24 11:42:49 +0800

    初步创建文件

---
+++ src/components/amplificationPicture/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  Box
+} from '@chakra-ui/core'
+
+const AmplificationPicture = ({ children, ...rest }) => {
+  // const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Box>789</Box>
+    </>
+  )
+}
+
+export default AmplificationPicture
---
+++ stories/amplificationPicture.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box } from '@chakra-ui/core'
+
+export default { title: 'AmplificationPicture(hui)' }
+
+export const AmplificationPicture = () => {
+  const darks = [
+    'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3988286801,48626279&fm=26&gp=0.jpg',
+    'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1947839005,1811220579&fm=26&gp=0.jpg',
+    'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3383916863,1183745965&fm=26&gp=0.jpg',
+    'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3383916863,1183745965&fm=26&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2407081651,3186896498&fm=26&gp=0.jpg',
+    'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=325461855,3402280707&fm=26&gp=0.jpg',
+    'https://vdposter.bdstatic.com/78e9d4347ddc538256c6eb3bced8e12b.jpeg?x-bce-process=image/resize,m_fill,w_242,h_182/format,f_jpg/quality,Q_100'
+  ]
+  return (
+    <Box>789745</Box>
+  )
+}
