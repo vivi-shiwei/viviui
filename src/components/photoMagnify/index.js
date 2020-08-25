6a1504d0299853c124c0a108fbd1a46907ebcc05
pengchihui1 <pengchihui1@gmail.com>
2020-08-25 09:17:36 +0800

    删除useRef

--- src/components/photoMagnify/index.js
+++ src/components/photoMagnify/index.js
@@ -1,13 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react'
 import {
   Box,
   Image,
   Modal,
-  ModalOverlay,
   ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
   ModalCloseButton,
   IconButton,
   Flex,
@@ -16,9 +12,7 @@ import {
 
 const photoMagnify = ({ isOpen, onClose, images = [], indexI = 0, children, ...props }) => {
   const [index, setIndex] = useState(indexI)
-  const flagRef = useRef(index);
-  flagRef.current = index;
-  
+
   // 左右边界
   if (index < 0) {
     setIndex(0)
@@ -44,7 +38,7 @@ const photoMagnify = ({ isOpen, onClose, images = [], indexI = 0, children, ...p
                 variantColor='teal'
                 icon='chevron-left'
                 borderRadius={9999}
-                onClick={() => { setIndex(index === 0 ? images.length - 1 : index - 1) ;console.log(flagRef.current)}}
+                onClick={() => { setIndex(index === 0 ? images.length - 1 : index - 1); console.log(flagRef.current) }}
                 visibility={index === 0 ? 'hidden' : 'visible'}
               />
             </Box>
@@ -68,7 +62,7 @@ const photoMagnify = ({ isOpen, onClose, images = [], indexI = 0, children, ...p
                 variantColor='teal'
                 icon='chevron-right'
                 borderRadius={9999}
-                onClick={() => { setIndex(index === images.length - 1 ? 0 : index + 1);console.log(flagRef.current) }}
+                onClick={() => { setIndex(index === images.length - 1 ? 0 : index + 1); console.log(flagRef.current) }}
                 visibility={index + 1 === images.length ? 'hidden' : 'visible'}
               />
             </Box>
